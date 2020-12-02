d0d58c3051abdf99321309072765d7d188450d6d
LSchultebraucks <L.Schultebraucks@t-online.de>
2020-12-02 18:44:38 +0100

    day2

---
+++ AdventOfCode2020/src/main/kotlin/day2/day2.kt
@@ -0,0 +1,30 @@
+import java.io.File
+
+private fun readInput(): List<String> {
+    return File("input_day2.txt").readLines();
+}
+
+fun main(args: Array<String>) {
+    var validPasswordCount = 0;
+    var validPasswordCountNew = 0;
+
+    var entries: List<String> = readInput();
+
+    for (entry in entries) {
+        val entryArgs = entry.split("-", " ", ":");
+        val min = Integer.parseInt(entryArgs[0])
+        val max = Integer.parseInt(entryArgs[1]);
+        val char: Char = entryArgs[2].single();
+        val word = entryArgs[4];
+        val occurencesCount = word.filter { it == char }.count();
+        if (occurencesCount in min..max) {
+            validPasswordCount++;
+        }
+        if ((word[min-1] == char && word[max-1] != char) || (word[min-1] != char && word[max-1] == char)) {
+            validPasswordCountNew++;
+        }
+    }
+
+    println("Part 1: $validPasswordCount Part 2: $validPasswordCountNew");
+}
+
